7c71b8434b0dbae7976aac759cd64a53fde69e87
jane <y412000@hotmail.com>
2023-01-20 18:00:13 +0800

    20230120 Jane update --> feat: modified Label component and add icon plugin

--- src/components/Form/Label.tsx
+++ src/components/Form/Label.tsx
@@ -1,35 +1,87 @@
-import { MouseEvent, ReactNode } from 'react';
+import React, { MouseEvent, ReactElement, ReactNode } from 'react';
 import styles from './Form.module.scss';
 
 interface LabelProps {
 	content: ReactNode | string;
 	forName: string;
-	children: ReactNode;
-	className?: string;
-	labelStyle?: 'row' | 'column';
+	icon?: JSX.Element;
+	iconPosition?: 'left' | 'right';
 	required?: boolean;
+	labelStyle?: 'row' | 'column';
+	childPosition?: 'inside' | 'outside';
+	childLocation?: 'top' | 'bottom';
+	className?: string;
+	children: ReactNode;
 	onClick?: (event: MouseEvent<HTMLLabelElement>) => void;
 }
 
 const Label: React.FC<LabelProps> = ({
 	content,
 	forName,
-	children,
-	className,
-	labelStyle = 'column',
+	icon,
+	iconPosition = 'left',
 	required = false,
+	labelStyle = 'column',
+	childPosition = 'inside',
+	childLocation = 'bottom',
+	className,
+	children,
 	onClick,
 }) => {
+	const iconReactObject = icon && React.Children.only(icon);
+	const clonedIcon =
+		iconReactObject &&
+		React.cloneElement(iconReactObject, {
+			size: '20',
+		});
+
+	if (childPosition === 'inside') {
+		return (
+			<div className={styles[`wrapper--${labelStyle}`]}>
+				<label
+					id={forName}
+					className={`w-full ${styles[`label--${labelStyle}`]} ${
+						className ?? ''
+					}`}
+					onClick={onClick}>
+					{childLocation === 'top' && children}
+
+					{clonedIcon ? (
+						<span
+							className={`${styles.icon} ${styles[`icon--${iconPosition}`]}`}>
+							{clonedIcon}
+						</span>
+					) : (
+						content
+					)}
+
+					{required && !clonedIcon && <sup className='text-error'>*</sup>}
+					{childLocation === 'bottom' && children}
+				</label>
+			</div>
+		);
+	}
+
 	return (
-		<div className={styles[`wrapper--${labelStyle}`]}>
+		<div
+			className={`${
+				clonedIcon ? styles[`wrapper--icon-${iconPosition}`] : ''
+			} ${styles[`wrapper--${labelStyle}`]}`}>
+			{childLocation === 'top' && children}
 			<label
 				id={forName}
 				className={`${styles[`label--${labelStyle}`]} ${className ?? ''}`}
 				onClick={onClick}>
-				{content}
-				{required && <sup className='text-error'>*</sup>}
+				{clonedIcon ? (
+					<span className={`${styles.icon} ${styles[`icon--${iconPosition}`]}`}>
+						{clonedIcon}
+					</span>
+				) : (
+					content
+				)}
+				{required && !clonedIcon && <sup className='text-error'>*</sup>}
 			</label>
-			{children}
+			{childLocation === 'bottom' && children}
 		</div>
 	);
 };
