5dc7bd7ade25658335466be752c72587269f0ce6
jane <y412000@hotmail.com>
2023-02-15 18:52:15 +0800

    20230215 Jane update --> feat: modified Checkbox component and add storybook

--- src/components/base/Form/Checkbox/Checkbox.tsx
+++ src/components/base/Form/Checkbox/Checkbox.tsx
@@ -8,11 +8,12 @@ export interface CheckboxProps extends Omit<InputToggleProps, 'type'> {}
  * Input Checkbox 基礎元件
  * @param name - input name
  * @param content - label content
- * @param checked - is initial input checked?
- * @param disabled - is disabled?
- * @param value - input value
- *
- * @param className - className for component
+ * @param defaultValue - input defaultValue
+
+ * @param checked - input checked
+ * @param defaultChecked - input defaultChecked
+
+ * @param className - className
  * @param checkedClassName - className for checked component
  * @param disablededClassName - className for disabled component
  *
@@ -21,12 +22,13 @@ export interface CheckboxProps extends Omit<InputToggleProps, 'type'> {}
  * @param fill - input icon style of checked
  * @param color - input icon color
  * @param hideIcon - hideIcon
+
  * @param onChange - callback of onChange event
  * @param afterChange - callback after isChecked state is changed(useEffect)
  * @returns
  */
 const Checkbox: React.FC<CheckboxProps> = (props) => {
-	return <InputToggle {...props} type='checkbox' fill={props.fill || true} />;
+	return <InputToggle {...props} type='checkbox' fill={props.fill ?? true} />;
 };
 
 export default Checkbox;
