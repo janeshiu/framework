4fc835d3f5663bccd17248565067860c4ce37299
jane <jane@jgallop.com>
2023-02-17 12:06:04 +0800

    20230217 Jane update --> feat: Label - modified how to use property in component and add storybook

---
+++ src/components/base/Form/Label/Label.stories.tsx
@@ -0,0 +1,30 @@
+import React, { useRef, useState } from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { default as StorybookComponent } from './Label';
+import Input from '../InputSeries/Input';
+
+type ComponentType = typeof StorybookComponent;
+
+export default {
+	component: StorybookComponent,
+	argTypes: {
+		content: {
+			control: 'text',
+		},
+	},
+	args: {},
+} as ComponentMeta<ComponentType>;
+
+const Template: ComponentStory<ComponentType> = (args) => (
+	<StorybookComponent {...args}>
+		<Input name='labelInput' placeholder='模擬用，Label 不含 <Input/>' />
+	</StorybookComponent>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+	content: 'Lable',
+	required: false,
+	href: '/',
+	linkContent: 'Link',
+};
--- src/components/base/Form/Label/Label.tsx
+++ src/components/base/Form/Label/Label.tsx
@@ -1,49 +1,45 @@
 import classNames from 'classnames';
 import React, { MouseEvent, ReactNode } from 'react';
 import styles from './Label.module.scss';
-import LabelTitle, { LabelTitleProps } from './LabelTitle';
+import LabelTitle, { LabelTitleProps } from './LabelTitle/LabelTitle';
 
 interface LabelProps extends LabelTitleProps {
 	children: ReactNode;
 
+	/** does label and children display inline or not */
 	row?: boolean;
+	/** is label and children reverse or not */
 	reverse?: boolean;
 
-	labelTitleClassName?: string;
 	className?: string;
+	/** labelTitle className */
+	labelTitleClassName?: string;
+
 	onClick?: (event: MouseEvent<HTMLLabelElement>) => void;
 }
 
 /**
  * Label - 使用時請包住 input element 或相關元件
  * @param children - Label children
- * @param row - 控制 label & children 是否以一行顯示
- * @param reverse - 控制 label & children 位置是否顛倒
- * @param labelTitleClassName - className for labelTitle
+ * @param row - does label and children display inline or not
+ * @param reverse - is label and children reverse or not
+ * @param labelTitleClassName - labelTitle className
  * @param className - className
  * @param onClick - onClick
  * @returns
  */
 const Label: React.FC<LabelProps> = ({
-	required = false,
-	size = 'normal',
 	children,
 	className,
 
-	row = false,
-	reverse = false,
+	row,
+	reverse,
 
 	onClick,
 
 	labelTitleClassName,
-	...labelTitleProps
+	...props
 }) => {
-	const LABEL_TITLE_PROPS = {
-		...labelTitleProps,
-		required,
-		size,
-	};
-
 	const baseClass = classNames({
 		[styles[`label`]]: true,
 		[styles[`row`]]: row,
@@ -52,10 +48,7 @@ const Label: React.FC<LabelProps> = ({
 
 	return (
 		<label className={`${baseClass} ${className ?? ''}`} onClick={onClick}>
-			<LabelTitle
-				{...LABEL_TITLE_PROPS}
-				className={`px-2 ${labelTitleClassName ?? ''}`}
-			/>
+			<LabelTitle {...props} className={`px-2 ${labelTitleClassName ?? ''}`} />
 			{children}
 		</label>
 	);
