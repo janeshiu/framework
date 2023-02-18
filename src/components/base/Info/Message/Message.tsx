c07228e4e68a04cbb24a184fde6b11ec977d723b
jane <jane@jgallop.com>
2023-02-18 10:41:44 +0800

    20230218 Jane update --> - feat: Message - add new props of messageType - feat: MessageGroup - order logic and function

--- src/components/base/Info/Message/Message.tsx
+++ src/components/base/Info/Message/Message.tsx
@@ -3,31 +3,40 @@ import { ReactNode } from 'react';
 import styles from './Message.module.scss';
 
 export interface MessageProps {
+	/** text of message */
 	msg: ReactNode;
+	/** size of message */
 	size?: SizeType;
-	type?: 'success' | 'error' | 'normal';
+	/** type of message */
+	type?: 'success' | 'error' | 'helper';
 	className?: string;
+	/** purpose of message */
+	messageType?: 'message' | 'hint';
 }
 
 /**
  * 一般表單底下所顯示的訊息的基礎元件
- * @param msg - 訊息內容
- * @param size - 尺寸
- * @param type - 訊息類型，'success' | 'error' | 'normal'
+ * @param msg - text of message
+ * @param size - size of message
+ * @param type - type of message，'success' | 'error' | 'normal'
  * @param className - className
+ * @param messageType - purpose of message
  * @returns
  */
 const Message: React.FC<MessageProps> = ({
 	msg,
 	size = 'normal',
-	type = 'normal',
+	type = 'helper',
+	messageType,
 	className,
 }) => {
+	const baseFontSize = messageType === 'hint' ? 'small' : 'normal';
+
 	return (
 		<small
-			className={`text-normal--${size} ${styles.message} ${styles[`${type}`]} ${
-				className ?? ''
-			}`}>
+			className={`text-${baseFontSize}--${size} ${styles.message} ${
+				styles[`${type}`]
+			} ${className ?? ''}`}>
 			{msg}
 		</small>
 	);
--- src/components/base/Info/Message/MessageGroup/MessageGroup.tsx
+++ src/components/base/Info/Message/MessageGroup/MessageGroup.tsx
@@ -1,22 +1,32 @@
 import { SizeType } from '@/types/style';
 import { ReactElement, ReactNode } from 'react';
-import Message from '../Message';
+import Message, { MessageProps } from '../Message';
 import styles from '../Message.module.scss';
 
 export interface MessageGroupProps {
 	className?: string;
 	size?: SizeType;
+	messageType?: MessageProps['messageType'];
 
+	/** does success message show or not */
 	success?: boolean;
+	/** success message  */
 	successMsg?: ReactNode;
+	/** success message className  */
 	successClass?: string;
 
+	/** does error message show or not */
 	error?: boolean;
+	/** error message  */
 	errorMsg?: ReactNode;
+	/** error message className  */
 	errorClass?: string;
 
+	/** does helper message show or not */
 	helper?: boolean;
+	/** helper message  */
 	helperMsg?: ReactNode;
+	/** helper message className  */
 	helperClass?: string;
 
 	children?: ReactElement<any, any>;
@@ -26,6 +36,8 @@ export interface MessageGroupProps {
  * 訊息包 - 可同時顯示 helper / error / success 相關訊息
  * @param className - className
  * @param size - 尺寸
+ * @param messageType - 訊息類型
+ *
  * @param success - 是否顯示 success 訊息
  * @param successMsg - success 訊息內容
  * @param successClass - success className
@@ -42,47 +54,41 @@ export interface MessageGroupProps {
 const MessageGroup: React.FC<MessageGroupProps> = ({
 	className,
 	size = 'normal',
+	messageType,
 	children = null,
 
-	success,
-	successMsg,
-	successClass,
-
-	error,
-	errorMsg,
-	errorClass,
-
-	helper,
-	helperMsg,
-	helperClass,
+	...props
 }) => {
 	const hasMessage =
-		(helper && helperMsg) || (error && errorMsg) || (success && successMsg);
+		hasTypeMessage('helper') ||
+		hasTypeMessage('error') ||
+		hasTypeMessage('success');
 
 	if (!hasMessage) return children;
+
+	function hasTypeMessage(type: MessageProps['type'] = 'helper') {
+		return props[type] && props[`${type}Msg`];
+	}
+
+	const renderMessage = (type: MessageProps['type'] = 'helper') => {
+		return (
+			hasTypeMessage(type) && (
+				<Message
+					size={size}
+					messageType={messageType}
+					msg={props[`${type}Msg`]}
+					className={props[`${type}Class`]}
+				/>
+			)
+		);
+	};
 
 	return (
 		<div className={`${styles.group} ${className ?? ''}`}>
 			{children}
-			{helper && helperMsg && (
-				<Message size={size} msg={helperMsg} className={helperClass} />
-			)}
-			{error && errorMsg && (
-				<Message
-					type='error'
-					size={size}
-					msg={errorMsg}
-					className={errorClass}
-				/>
-			)}
-			{success && successMsg && (
-				<Message
-					type='success'
-					size={size}
-					msg={successMsg}
-					className={successClass}
-				/>
-			)}
+			{renderMessage('helper')}
+			{renderMessage('error')}
+			{renderMessage('success')}
 		</div>
 	);
 };
